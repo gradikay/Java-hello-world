97fe25115cd829095e721d374b8c804904079fed
GRADI <56418645+gradikay@users.noreply.github.com>
2024-05-19 00:08:16 -0400

    Create helloworld.java

---
+++ helloworld.java
@@ -0,0 +1,10 @@
+// This is a simple Java program that prints "Hello, World!" to the console.
+public class HelloWorld {
+
+    // The main method is the entry point of the program.
+    // It is called when the program starts.
+    public static void main(String[] args) {
+        // This line prints "Hello, World!" to the console.
+        System.out.println("Hello, World!");
+    }
+}
